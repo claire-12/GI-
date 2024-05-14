54f83832cdc218103b92d97a95caf36595a9e1a8
Louis Pixodeo <louis@pixodeo.net>
2024-05-14 17:03:16 +0700

    GID-1034
    Signed-off-by: Louis Pixodeo <louis@pixodeo.net>

--- wp-content/plugins/webshop-custom-functions/includes/classes/GIWoocommerce.php
+++ wp-content/plugins/webshop-custom-functions/includes/classes/GIWoocommerce.php
@@ -12,6 +12,8 @@ class GIWoocommerce
         add_action('wp_footer', array($this, 'gi_woocommerce_add_address_modal'));
         add_action('wp_ajax_gi_update_shipping_address', array($this, 'gi_update_shipping_address_callback'));
         add_action('wp_ajax_gi_get_modal_address_content', array($this, 'gi_get_modal_address_content_callback'));
+        add_action('woocommerce_checkout_terms_and_conditions', array($this, 'gi_woocommerce_add_wp_form_9'));
+        add_action('woocommerce_checkout_update_order_review', array($this, 'gi_woocommerce_checkout_update_order_meta'));
 
         add_filter('woocommerce_return_to_shop_redirect', array($this, 'gi_woocommerce_return_to_shop_redirect'));
         add_filter('woocommerce_checkout_must_be_logged_in_message', array($this, 'gi_woocommerce_checkout_is_not_logged'));
@@ -173,6 +175,29 @@ class GIWoocommerce
         wc_get_template('template-parts/checkout/form-address.php', ['address' => $address, 'address_key' => $_REQUEST['address_key']], '', WBC_PLUGIN_DIR);
         $content = ob_get_clean();
         wp_send_json_success($content);
+    }
+    public function gi_woocommerce_add_wp_form_9()
+    {
+        $customer_level = get_customer_level(get_current_user_id());
+        //$customer_level = 1;
+        if ($customer_level === 1) {
+            wc_get_template('template-parts/checkout/form-wp9.php', [], '', WBC_PLUGIN_DIR);
+        }
+    }
+    public function gi_woocommerce_checkout_update_order_meta()
+    {
+        if (!empty($_FILES['form-w9']['name'])) {
+            WC()->cart->remove_taxes();
+            /*$upload = wp_upload_bits($_FILES['form-w9']['name'], null, file_get_contents($_FILES['form-w9']['tmp_name']));
+            if(isset($upload['error']) && $upload['error'] != 0) {
+                return;
+            } else {
+                update_post_meta($order_id, 'user_wp9_form', $upload);
+                // Remove all tax calculations
+                remove_all_actions('woocommerce_calc_tax');
+                remove_all_actions('woocommerce_after_order_total_tax');
+            }*/
+        }
     }
 }
 
