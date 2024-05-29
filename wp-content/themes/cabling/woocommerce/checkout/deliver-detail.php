86e04ce32671a78102004f22d5ec08f6bf44fa6f
Nuno <nuno@infolabix.com>
2024-05-29 11:51:10 +0700

    nuno-dev/GID-1064 complete task

--- wp-content/themes/cabling/woocommerce/checkout/deliver-detail.php
+++ wp-content/themes/cabling/woocommerce/checkout/deliver-detail.php
@@ -18,14 +18,25 @@ defined( 'ABSPATH' ) || exit;
 $customer_id = get_current_user_id();
 $address_type = "shipping";
 $addresses = THMAF_Utils::get_custom_addresses($customer_id, $address_type);
-//var_dump($custom_addresses);
+
+$default_shipping = '';
+$custom_address = get_user_meta($customer_id, THMAF_Utils::ADDRESS_KEY);
+if( count($custom_address) ){
+    foreach( $custom_address as $custom_addres ){
+        if( isset( $custom_addres['default_shipping'] ) ){
+            $default_shipping = $custom_addres['default_shipping'];
+        }
+    }
+}
 ?>
 <div class="woocommerce-shipping-blocks mt-3">
     <?php if (!empty($addresses)): ?>
         <?php foreach($addresses as $address_key => $address): ?>
             <?php $address_key_param = "'".$address_key."'"; ?>
             <div class="address-item d-flex p-2 mb-3" data-address-key="<?php echo $address_key_param ?>" data-address='<?php echo wp_json_encode($address) ?>' >
-                <input type="radio" value="<?php echo $address_key_param ?>" name="select-shipping-address" class="form-check" data-type="<?php echo $address_type ?>" data-id=<?php echo $address_key_param ?>>
+                <input type="radio" 
+                <?= ( $default_shipping == $address_key ) ? 'checked' : '' ?>
+                value="<?php echo $address_key_param ?>" name="select-shipping-address" class="form-check" data-type="<?php echo $address_type ?>" data-id=<?php echo $address_key_param ?>>
                 <div class="address-details ms-3">
                     <h4><?php echo $address['shipping_company']?></h4>
                     <ul>
