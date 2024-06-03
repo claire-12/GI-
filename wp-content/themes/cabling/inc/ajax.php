34df660f153d16e465c7103e62df1e4fb73e47d1
Jose <jmartinspc@gmail.com>
2024-06-03 14:54:17 +0100

    small changes
    Changed SAP calls to allow part number to be just last 3 digits
    changes in text

--- wp-content/themes/cabling/inc/ajax.php
+++ wp-content/themes/cabling/inc/ajax.php
@@ -1058,12 +1058,16 @@ function cabling_get_api_ajax_callback()
                 if (empty($value)) {
                     continue;
                 }
+                if($name=='OldMaterialNumber'){
+                    $value=str_pad(str_replace('-','',$value),7,'0568',STR_PAD_LEFT);
+                }
                 $bodyParams[] = array(
                     'Field' => $name,
                     'Value' => $value,
                     'Operator' => 'and',
                 );
             }
+
 
             $type = 'ZDD_I_SD_PIM_MaterialBacklog';
             $type_level_2 = 'ZDD_I_SD_PIM_MaterialBacklogType';
@@ -1073,6 +1077,8 @@ function cabling_get_api_ajax_callback()
                     $apiStockEndpoint = $apiEndpointBasic . 'GET_DATA_STOCK_CDS';
                     $template = $data['api_page'] . '-item.php';
                     $oldMaterialNumber = $data['api']['MaterialOldNumber'];
+                    //JM 20240603 allow search by dashnumber
+                    $oldMaterialNumber= str_pad(str_replace('-','',$oldMaterialNumber),7,'0568',STR_PAD_LEFT);
                     $material = $data['api']['Material'];
                     $basicMaterial = $data['api']['BasicMaterial'];
 
